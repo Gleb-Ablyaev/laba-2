756eaf23f85b4c7ce5df0ea392080bbaf3ecf043
Gleb-Ablyaev <99827453+Gleb-Ablyaev@users.noreply.github.com>
2022-04-19 00:45:16 +0400

    Update laba 2.py

--- laba 2.py
+++ laba 2.py
@@ -1,36 +1,39 @@
-import time
+import re
+buffer_len = 1
+work_buffer = ""
+b = ""
 
 try:
     while 1:
         print("\n введите число k")
         number = (input("k="))
-        if number >= '0' and number <= '9':
+        if '0' <= number <= '9':
             k = int(number)
             break
         else:
             print('это не цифра')
-    start=time.time()
-    with open("laba 2.txt","r") as file:
-        print("\n результат работы программы \n")
-        data=file.read()
 
-        if not data:
+    with open("laba 2.txt", "r") as file:
+        print("\n результат работы программы \n")
+        buffer = file.read(buffer_len)
+
+        if not buffer:
             print("\n рабочий файл пустой, измените содержание файла")
 
-        if data:
-            mass = data.split('.')
-            mass2 = []
-            i = 1
+        while buffer:
+            work_buffer += buffer
+            if re.findall(r'[.!?]', buffer):
+                b = re.split(r'\W', work_buffer)
+                b = b[:len(b)-1]
+                if len(b) == k:
+                    print(work_buffer)
+                g = ""
+                work_buffer = ""
 
-            print("\n предложения согласно условию:")
-            for i in range(0, len(mass)):
-                mass2.append(mass[i].split())
-                if len(mass2[i]) == k:
-                    print(mass[i], '.')
+            buffer = file.read(buffer_len)
+        if re.findall(r'[^.!?]', work_buffer):  # Если буфер переполнен и нет окончания предложения
+            print("\nФайл *.txt не содержит знаков окончания предложения.\nОткорректируйте файл *.txt в директории или переименуйте существующий *.txt файл.")
 
-    finish=time.time()
-    result=finish-start
-    print("program time: " + str(result) + "seconds.")
 
 except FileNotFoundError:
-    print("\n файл laba 2.txt в директории проекта не обнаружен.\n Добавьте файл в директорию или переименуйте существующий")
+    print("\n файл *.txt в директории проекта не обнаружен.\n Добавьте файл в директорию или переименуйте существующий")
